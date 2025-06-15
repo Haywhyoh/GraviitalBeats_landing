a6419d017739c0043ad094e39720ca1d8d29fa21
Ayomide <adefeyisayo998@gmailcom>
2025-06-15 15:32:33 +0100

    Add favicon and logo images; update index.html to include favicon link

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,8 @@ const Logo: React.FC = () => {
   return (
     <div className="flex items-center gap-2 font-montserrat">
       <span className="text-2xl font-extrabold">
-        Gravii<span className="beat-waveform">tal</span>Beats
+        <img src="/images/logo.png" alt="GraviitalBeats" className="w-10 h-10" />
+        {/* Gravii<span className="beat-waveform">tal</span>Beats */}
       </span>
     </div>
   );
