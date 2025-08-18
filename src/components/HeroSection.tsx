7b92b7bc7587e5385483f47627c6e07b386bc711
Ayomide <adefeyisayo998@gmail.com>
2025-08-18 23:02:08 +0100

    Remove top padding from HeroSection for improved layout consistency

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,7 +17,7 @@ const HeroSection: React.FC = () => {
   };
 
   return (
-    <section className="relative min-h-screen flex items-center pt-20">
+    <section className="relative min-h-screen flex items-center">
       {/* Main Background Image */}
       <div className="absolute inset-0 z-0">
         <img 
